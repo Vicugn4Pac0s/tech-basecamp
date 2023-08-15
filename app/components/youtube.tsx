5f00473d42c9abe891633f7a77559b4d9502f104
YOSHIKI <vicugnapacos.g@gmail.com>
2023-08-16 00:01:46 +0900

    動画の切り替えを追加

--- app/components/youtube.tsx
+++ app/components/youtube.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { initYouTubeAPI, onYoutubeReady } from "../libs/youtube";
 
 declare global {
@@ -14,21 +14,40 @@ type Props = {
 };
 
 export default function Youtube({ id }: Props) {
-  let player: YT.Player;
-  initYouTubeAPI()
-  onYoutubeReady(()=> {
-    player = new YT.Player(`yt-video`, {
-      videoId: id
-    })
-  })
+  const [player, setPlayer] = useState<YT.Player | null>(null);
+  const [ytId, setYtId] = useState(id);
 
-  useEffect(()=> {
-    player?.cueVideoById(id)
-  }, [id])
+  initYouTubeAPI();
+  onYoutubeReady(() => {
+    const newPlayer = new YT.Player(`yt-video`, {
+      videoId: id,
+    });
+    setPlayer(newPlayer);
+  });
+
+  useEffect(() => {
+    if (player && player.cueVideoById) {
+      player.cueVideoById(ytId);
+    }
+  }, [ytId, player]);
 
   return (
     <div>
-      <div id={`yt-video`}></div>      
+      <div id={`yt-video`}></div>
+      <div
+        onClick={() => {
+          setYtId("Xw9hHVFBZUI");
+        }}
+      >
+        Xw9hHVFBZUI
+      </div>
+      <div
+        onClick={() => {
+          setYtId("r6MoN5FWpoI");
+        }}
+      >
+        r6MoN5FWpoI
+      </div>
     </div>
   );
 }
