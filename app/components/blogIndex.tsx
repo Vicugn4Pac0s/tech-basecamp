4f600017c6166f9fc9599f28014f52e93d7b092b
YOSHIKI <vicugnapacos.g@gmail.com>
2023-08-17 22:53:52 +0900

    タグの絞り込み追加

--- app/components/blogIndex.tsx
+++ app/components/blogIndex.tsx
@@ -9,21 +9,31 @@ import BlogTagList from "./blogTagList";
 export default function BlogIndex() {
   const [articles, setArticles] = useState<BlogArticle[]>([])
   const [tags, setTags] = useState<BlogTag[]>([])
+  const [currentTagId, setCurrentTagId] = useState('')
 
   useEffect(() => {
     async function fetchData() {
-      const blogs = await getBlogsData({
-      });
+      let blogQuery = {}
+      if(currentTagId !== '') {
+        blogQuery = {
+          and: [
+            {
+              tags: currentTagId
+            }
+          ]
+        }
+      }
+      const blogs = await getBlogsData(blogQuery);
       setArticles(blogs.items);
       const tags = await getBlogTag();
       setTags(tags.items);
     }
     fetchData();
-  }, []);
+  }, [currentTagId]);
 
   return (
     <div>
-      <BlogTagList tags={tags}></BlogTagList>
+      <BlogTagList tags={tags} setCurrentTagId={setCurrentTagId}></BlogTagList>
       <BlogList articles={articles}></BlogList>
     </div>
   )
--- app/components/blogTagList.tsx
+++ app/components/blogTagList.tsx
@@ -1,17 +1,24 @@
 "use client";
 
+import { Dispatch, SetStateAction } from "react";
 import { BlogTag } from "../types/blog";
 import TagBtn from "./tagBtn";
 
 type props = {
   tags: BlogTag[]
+  setCurrentTagId: Dispatch<SetStateAction<string>>
 }
 
-export default function BlogTagList({tags}: props) {
+export default function BlogTagList({tags, setCurrentTagId}: props) {
+
+  const setTag = (tagId: string) => () => {
+    setCurrentTagId(tagId)
+  }
+  
   return (
     <div className="flex flex-wrap gap-2 mb-8">
       {tags.map(item => (
-        <div>
+        <div key={item._id} onClick={ setTag(item._id) }>
           <TagBtn name={item.name}></TagBtn>
         </div>
       ))}
