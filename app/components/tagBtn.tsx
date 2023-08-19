152fb922b53393bb19d9919c1c3a000dc53c1bd2
Vicugn4Pac0s <vicugnapacos.g@gmail.com>
2023-08-19 21:59:01 +0900

    borderradius

--- app/components/tagBtn.tsx
+++ app/components/tagBtn.tsx
@@ -6,6 +6,6 @@ type props = {
 
 export default function TagBtn({name}: props) {
   return (
-    <div className="bg-[#eee] py-1 px-2 cursor-pointer hover:bg-black hover:text-white">{name}</div>
+    <div className="bg-[#eee] py-1 px-2 rounded cursor-pointer hover:bg-black hover:text-white">{name}</div>
   )
 }
