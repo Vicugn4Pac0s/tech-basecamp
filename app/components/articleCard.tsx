c5f7b8b453c792a9c3fd4d673011523a0fa07db5
Vicugn4Pac0s <vicugnapacos.g@gmail.com>
2023-08-19 23:03:52 +0900

    youtube.comの時リンクしないよう

--- app/components/articleCard.tsx
+++ app/components/articleCard.tsx
@@ -12,12 +12,21 @@ export default function ArticleCard({ item }: Props) {
     imgPath = item.coverImage.src;
   }
 
+  const clickLink = (articleUrl: string) => (event: React.MouseEvent) => {
+    const articleDomain = extractDomain(articleUrl)
+    if(articleDomain === 'youtube.com') {
+      event.preventDefault();
+      alert(articleDomain)
+    }
+  }
+  
   return (
     <article key={item._id} className="h-full w-full relative rounded overflow-hidden">
       <a
         href={item.articleUrl}
         target="_blank"
         className="block h-full p-3 hover:bg-[#f9f9f9]"
+        onClick={clickLink(item.articleUrl)}
       >
         <div className="mb-2">
           <Image src={imgPath} height={512} width={1280} alt="" className="h-[100px] object-cover"></Image>
