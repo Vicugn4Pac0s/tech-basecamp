e71fcdb842d8ca3c13c4f52679cca503b3b4b17f
YOSHIKI <vicugnapacos.g@gmail.com>
2023-08-19 16:50:12 +0900

    ロゴにリンクを

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="bg-[#eee]">
       <div className="p-5">
-        <Image src="/logo.svg" alt="" height="19" width="202"></Image>
+        <Link href="/">
+          <Image src="/logo.svg" alt="" height="19" width="202"></Image>
+        </Link>
       </div>
     </header>
   );
