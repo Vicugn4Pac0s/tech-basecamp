7ef7eefc4746de4ce5c43a530e268cc29d198d0e
Vicugn4Pac0s <vicugnapacos.g@gmail.com>
2023-08-19 22:08:55 +0900

    topにメディア一覧追加

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,16 @@ import Link from "next/link";
 import HeadingA from "./components/headingA";
 import MediaList from "./components/mediaList";
 import BtnA from "./components/btnA";
+import { getMediasData } from "./libs/client";
 
-export default function Home() {
+export default async function Home() {
+  const articles = await getMediasData({
+    limit: 6
+  })
   return (
     <main>
       <HeadingA>メディア一覧</HeadingA>
-      <div className="">記事一覧</div>
+      <MediaList articles={articles.items} limit={8}></MediaList>
       <div className="text-center mt-4">
         <BtnA href="/media">もっとみる</BtnA>
       </div>
