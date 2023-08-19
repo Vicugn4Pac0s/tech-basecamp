7579e2dbc301c5c2ea5d527c71decf7e4e4e58d3
Vicugn4Pac0s <vicugnapacos.g@gmail.com>
2023-08-19 21:20:24 +0900

    header のbg設定

--- app/components/mv.tsx
+++ app/components/mv.tsx
@@ -3,13 +3,14 @@ import Inner from "./inner";
 
 type props = {
   title: string,
-  description?: string
+  description?: string,
+  bgSrc?: string 
 }
 
-export default function Mv({title, description}: props) {
+export default function Mv({title, description, bgSrc}: props) {
   return (
-    <div className="bg-black h-[200px]">
-      <Inner className="flex justify-center items-center h-full w-full">
+    <div className="bg-black h-[200px] relative overflow-hidden">
+      <Inner className="flex justify-center items-center h-full w-full relative z-10">
         <div className="text-white text-center">
           <h1 className="text-3xl mb-4">{title}</h1>
           <p>
@@ -17,6 +18,9 @@ export default function Mv({title, description}: props) {
           </p>
         </div>
       </Inner>
+      <div className="bg-black absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 h-full w-full">
+        <Image src={bgSrc? bgSrc: '/mv.jpg'} alt="" height={300} width={2880} className="opacity-50 h-full object-cover"></Image>
+      </div>
     </div>
   );
 }
